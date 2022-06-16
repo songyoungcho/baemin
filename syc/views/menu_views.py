2ddd97ccb5242e1138f89a56103ef900589b1b00
songyoungcho <syh225@gmail.com>
2022-06-16 23:01:11 +0900

    modi dele store menu

--- syc/views/menu_views.py
+++ syc/views/menu_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, request,render_template,g
+from flask import Blueprint, url_for, request,render_template,g, flash
 from werkzeug.utils import redirect
 
 from syc import db
@@ -26,3 +26,32 @@ def create_menu(store_id):
             db.session.commit()
             return redirect(url_for('store.detail', store_id=store_id))
     return render_template('my/create_menu.html',  form=form)
+
+@bp.route('/modify/<int:menu_id>', methods=('GET', 'POST'))
+@login_required
+def modify(menu_id):
+    menu = Menu.query.get_or_404(menu_id)
+    if g.user != menu.user:
+        flash('수정권한이 없습니다')
+        return redirect(url_for('store.detail', store_id=menu.store.id))
+    if request.method == "POST":
+        form = MenuCreateForm()
+        if form.validate_on_submit():
+            form.populate_obj(menu)
+            db.session.commit()
+            return redirect(url_for('store.detail', store_id=menu.store.id))
+    else:
+        form = MenuCreateForm(obj=menu)
+    return render_template('my/create_menu.html', form=form)
+
+@bp.route('/delete/<int:menu_id>')
+@login_required
+def delete(menu_id):
+    menu = Menu.query.get_or_404(menu_id)
+    store_id = menu.store.id
+    if g.user != menu.user:
+        flash('삭제권한이 없습니다')
+    else:
+        db.session.delete(menu)
+        db.session.commit()
+    return redirect(url_for('store.detail', store_id=store_id))
